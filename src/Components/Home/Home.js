0c56b6299c5b589c1a9b12f86cd22bbb48d43f39
shabnam-priyanka <shabnam.priyanka@live.ca>
2022-06-12 15:49:03 -0400

    I started again

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -6,30 +6,35 @@ import './Home.css';
 const Home = (props) => {
    const {storeText, setStoreText} = props;
     console.log(storeText);
-        const [textInput, setTextInput] = useState('');
+    const initialState = {title: '', isComplete: false};
+        const [textInput, setTextInput] = useState(initialState);
        
         const handleText = (e) => {
             e.preventDefault();
-            setTextInput(e.target.value)
+            const titleCopy = {...textInput};
+            titleCopy.title = e.target.value;
+            titleCopy.myKey = new Date() * 1; 
+            setTextInput(titleCopy)
             
         }
 
         const handleSubmit = () =>{
             setStoreText([...storeText, textInput]);
+            setTextInput(initialState);
         }
 
     return (
         <div>
             <Header></Header>
-            <input type="text" name="text" id=""  onChange= {handleText} />
+            <input type="text" name="text" id="" value={textInput.title} onChange= {handleText} />
             <input type="button" value="submit" onClick={handleSubmit} />
 
             <div className="homeStyle">
             {
-                storeText.map((a , index) => {
-                    return <li key = {index}><button >Delete</button>
+                storeText.map((a ) => {
+                    return <li key = {a.myKey}><button >Delete </button>
                     <button >Complete</button> 
-                    <span className='style'> {a}</span> 
+                    <span className='style'> {a.title}</span> 
                     
                     </li>;
                 })
